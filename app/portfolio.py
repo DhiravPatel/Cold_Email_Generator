67f2800d2b4b32c83351c558fac999133b26b83f
dhiravpatel <dhiravpatel07@gmail.com>
2025-03-01 17:40:48 +0530

    Remove chromadb

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import chromadb
+# import chromadb
 import uuid
 
 
@@ -10,16 +10,16 @@ class Portfolio:
     #     self.chroma_client = chromadb.PersistentClient('vectorstore')
     #     self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
 
-    def load_portfolio(self, uploaded_file):
-        self.uploaded_file = uploaded_file
-        self.data = pd.read_csv(uploaded_file)
-        self.chroma_client = chromadb.PersistentClient('vectorstore')
-        self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
-        if not self.collection.count():
-            for _, row in self.data.iterrows():
-                self.collection.add(documents=row["Techstack"],
-                                    metadatas={"links": row["Links"]},
-                                    ids=[str(uuid.uuid4())])
+    # def load_portfolio(self, uploaded_file):
+    #     self.uploaded_file = uploaded_file
+    #     self.data = pd.read_csv(uploaded_file)
+    #     self.chroma_client = chromadb.PersistentClient('vectorstore')
+    #     self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
+    #     if not self.collection.count():
+    #         for _, row in self.data.iterrows():
+    #             self.collection.add(documents=row["Techstack"],
+    #                                 metadatas={"links": row["Links"]},
+    #                                 ids=[str(uuid.uuid4())])
 
     def query_links(self, skills):
         print(skills)
